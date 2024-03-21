ee2dc08d9d828bb6e4c2a123e84543837c28fdd3
Charlie Oliver <co@perenna.co.uk>
2024-03-21 09:02:59 +0100

    feat: use splitkb vial config, get rgb working

--- keyboards/splitkb/aurora/corne/keymaps/vial/config.h
+++ keyboards/splitkb/aurora/corne/keymaps/vial/config.h
@@ -1,72 +1,16 @@
-/* Copyright 2022 splitkb.com <support@splitkb.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* Copyright 2023 splitkb.com <support@splitkb.com> */
 
 #pragma once
 
-/* -- qmk features --*/
-#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define VIAL_KEYBOARD_UID {0x89, 0x87, 0xB7, 0x0F, 0x54, 0x23, 0xC2, 0xE8}
 
-/* -- vial -- */
-#define VIAL_KEYBOARD_UID { 0xD1, 0x12, 0x95, 0xF4, 0x8E, 0xC8, 0xA1, 0x12 }
 #define VIAL_UNLOCK_COMBO_ROWS { 3, 7 }
 #define VIAL_UNLOCK_COMBO_COLS { 4, 4 }
-#define VIAL_TAP_DANCE_ENTRIES 128
-
-/* -- split -- */
-#define SPLIT_HAND_PIN F4
-
-/* -- oled -- */
-#ifdef OLED_ENABLE
-#    define OLED_DISPLAY_128X32
-#    define SPLIT_OLED_ENABLE
-#endif
-
-/* -- rgb per key -- */
-#ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 128
-#    define RGB_MATRIX_LED_COUNT 54
-#    define RGB_MATRIX_SPLIT { 27, 27 }
-#    define SPLIT_TRANSPORT_MIRROR
-#    define RGB_MATRIX_KEYPRESSES // reacts to keypresses
-// #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
-// #   define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-// #   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-#    define RGB_MATRIX_HUE_STEP 8
-#    define RGB_MATRIX_SAT_STEP 8
-#    define RGB_MATRIX_VAL_STEP 8
-#    define RGB_MATRIX_SPD_STEP 10
-
-/* Enable the animations you want/need.  You may need to enable only a small number of these because       *
- * they take up a lot of space.  Enable and confirm that you can still successfully compile your firmware. */
-// RGB Matrix Animation modes. Explicitly enabled
-// For full list of effects, see:
-// https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
-// enabled only if RGB_MATRIX_FRAMEBUFFER_EFFECTS is defined
-#    define ENABLE_RGB_MATRIX_TYPING_HEATMAP
-// enabled only of RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES is defined
-#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-#    define ENABLE_RGB_MATRIX_SOLID_SPLASH
-#    define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
-#endif
 
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #define RGB_MATRIX_KEYPRESSES
 
-/* -- rgb underglow -- */
-#define BACKLIGHT_BREATHING
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define VIAL_COMBO_ENTRIES 128
+#define VIAL_TAP_DANCE_ENTRIES 128
--- keyboards/splitkb/aurora/corne/keymaps/vial/rules.mk
+++ keyboards/splitkb/aurora/corne/keymaps/vial/rules.mk
@@ -1,37 +1,22 @@
 # -- controller -- #
 CONVERT_TO = liatris
 
-# -- encoder -- #
-ENCODER_ENABLE = no
-
-# -- oled -- #
-OLED_ENABLE = yes
-
-# -- rgb leds -- #
-RGB_MATRIX_ENABLE = yes
-# RGB_MATRIX_CUSTOM_USER = yes
-WS2812_DRIVER = vendor
-
 # -- gui editor support -- #
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 
 # -- vial -- #
-# VIAL_INSECURE = yes
+VIAL_INSECURE = yes
 VIALRGB_ENABLE = yes
 
 # -- qmk features -- #
-KEY_OVERRIDE_ENABLE = no
-SECURE_ENABLE = no
-
-AUTO_SHIFT_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 LEADER_ENABLE = yes
 KEY_LOCK_ENABLE = yes
-MOUSEKEY_ENABLE = yes
-OS_DETECTION_ENABLE = yes
-RAW_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
 SEND_STRING_ENABLE = yes
 TRI_LAYER_ENABLE = yes
 WPM_ENABLE = yes
+
+# KEY_OVERRIDE_ENABLE = no
+# SECURE_ENABLE = no
