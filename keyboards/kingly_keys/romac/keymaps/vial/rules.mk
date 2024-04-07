8d3a779fcd7fb029354c44c712afe189e74bbb65
Charlie Oliver <co@perenna.co.uk>
2024-04-07 21:29:39 +0100

    feat: add config for romac

--- keyboards/kingly_keys/romac/keymaps/vial/rules.mk
+++ keyboards/kingly_keys/romac/keymaps/vial/rules.mk
@@ -1,3 +1,17 @@
+# -- firmware -- #
+CONVERT_TO = liatris
+LTO_ENABLE = yes
+
+# -- gui editor support -- #
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
-LTO_ENABLE = yes
+
+# -- vial -- #
+VIAL_INSECURE = yes
+
+# -- qmk features -- #
+LEADER_ENABLE = yes
+KEY_LOCK_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+SECURE_ENABLE = no
+SEND_STRING_ENABLE = yes
